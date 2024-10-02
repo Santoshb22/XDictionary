eb1d0408ca85b5939c7f452270ff5e56865aefe4
Santoshb22 <sb289848@gmail.com>
2024-10-02 15:09:59 +0530

    re-deploy

--- x-dictionary/src/Component/XDictionary.jsx
+++ x-dictionary/src/Component/XDictionary.jsx
@@ -1,47 +1,47 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const XDictionary = ({dictionary}) => {
-    const [defination, setDefination] = useState("");
-    const [inputSearch, setInputSearch] = useState("");
-  
+const XDictionary = ({ dictionary }) => {
+  const [definition, setDefinition] = useState("");
+  const [inputSearch, setInputSearch] = useState("");
 
-    const handleSearch = () => {
-            if (!inputSearch) {
-                setDefinition("Word not found in the dictionary.");
-                return;
-              }
-        }   
-
-        const word = dictionary.find(data => data.word.toLowerCase() === inputSearch);
-        if(word) {
-            setDefination(word.meaning);
-        } else {
-            setDefination("Word not found in the dictionary.")
-        }
+  const handleSearch = () => {
+    if (!inputSearch) {
+      setDefinition("Word not found in the dictionary.");
+      return;
     }
+
+    const word = dictionary.find(
+      (data) => data.word.toLowerCase() === inputSearch
+    );
+    if (word) {
+      setDefinition(word.meaning);
+    } else {
+      setDefinition("Word not found in the dictionary.");
+    }
+  };
 
   return (
     <div>
-        <h1>Dictionary App</h1>
+      <h1>Dictionary App</h1>
 
-        <div className="input-box">
-            <input 
-            type="text"
-            placeholder="Search for a word..."
-            onChange = {(e) => setInputSearch(e.target.value.toLowerCase())}
-            value={inputSearch}
-            />
-            <button onClick={handleSearch} >Search</button>
+      <div className="input-box">
+        <input
+          type="text"
+          placeholder="Search for a word..."
+          onChange={(e) => setInputSearch(e.target.value.toLowerCase())}
+          value={inputSearch}
+        />
+        <button onClick={handleSearch}>Search</button>
 
-            <br />
-            <b>Definition: </b>
-        </div>
+        <br />
+        <b>Definition: </b>
+      </div>
 
-        <div className="defination">
-            <p>{defination}</p>
-        </div>
+      <div className="definition">
+        <p>{definition}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default XDictionary
+export default XDictionary;
